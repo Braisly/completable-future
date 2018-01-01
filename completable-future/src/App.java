34007404cc9b0b5067b51cf6088aca4bc571117e
braisly <brais_69_bly@hotmail.com>
2018-01-01 23:21:57 +0100

    CompletableFuture runAsync.

---
+++ completable-future/src/App.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+	
+	
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		
+		// Using Lambda Expression
+		CompletableFuture
+				.runAsync(()->{
+			        	try {
+							TimeUnit.SECONDS.sleep(1); 
+						} catch (Exception e) {
+					        e.printStackTrace();
+					        }
+						System.out.println("RUNNING");
+
+					})
+				.thenAccept((i)-> {
+						System.out.println("LAST");
+					}
+				);
+		System.out.println("FIRST");
+		//Without a sleep the app is over
+		TimeUnit.SECONDS.sleep(3);
+
+		
+	}
+
+}
